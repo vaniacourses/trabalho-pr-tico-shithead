e440a71d9c3e1d521a91ae66f7bc191d71603c45
VampirePidgeon <141532297+Pombo-Programador@users.noreply.github.com>
2024-07-03 17:42:02 -0300

    Conserta erro de CORS

--- Entrega2/backend/app/main.py
+++ Entrega2/backend/app/main.py
@@ -1,12 +1,28 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
+from fastapi.middleware.cors import CORSMiddleware
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Dependency
 def get_db():
